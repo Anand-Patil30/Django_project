4a15e58507c2e57e3e2df6c8fafc83e301c5a8a5
Anand Patil <patilanand342@gmail.com>
2023-10-22 20:59:10 +0530

    APi of the course
    Update settings.py

--- OLP/settings.py
+++ OLP/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,26 @@ SIMPLE_JWT={
     'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
     'ROTATE_REFRESH_TOKEN':False,
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_THHROTTLE_CLASSES':[
+        'rest_framework.throttling.AnonRateThrottle', #unauthenticated users can make up to 30 requests per minute
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES':{
+        'register':'6/minute', #registration rate limit
+        'anon':'100/day',
+        'user':'100/day', 
+    }
+}
+
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    'ROTATE_REFRESH_TOKEN':False,
+}
 
 # Application definition
 
@@ -61,6 +82,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 ]
